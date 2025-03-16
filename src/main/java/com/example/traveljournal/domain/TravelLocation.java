f629d7625968e31112db594a5ef21bcf80dbf4b2
Silea Horia <horia.silea@gmail.com>
2025-03-16 14:16:38 +0200

    Added the TravelLocation entity

---
+++ src/main/java/com/example/traveljournal/domain/TravelLocation.java
@@ -0,0 +1,33 @@
+package com.example.traveljournal.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "TravelLocation")
+public class TravelLocation {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String dateVisited;
+    private Integer rating;
+    private String imagePath;
+    private String description;
+
+
+}
