4c6c8fce2661b0305378a5dd28735a9f646fc34d
Silea Horia <horia.silea@gmail.com>
2025-03-16 14:20:53 +0200

    Created Repository Interface

---
+++ src/main/java/com/example/traveljournal/repository/Repository.java
@@ -0,0 +1,11 @@
+package com.example.traveljournal.repository;
+
+import com.example.traveljournal.domain.TravelLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+@org.springframework.stereotype.Repository
+public interface Repository extends JpaRepository<TravelLocation, String> {
+    Optional<TravelLocation> findById(String id);
+}
