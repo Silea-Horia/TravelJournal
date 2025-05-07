63a3bc25e80af90cf4b241a3d5aeebed12d05e95
Silea Horia <horia.silea@gmail.com>
2025-05-07 17:12:51 +0300

    Created CountryRepository

---
+++ src/main/java/com/example/traveljournal/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.example.traveljournal.repository;
+
+import com.example.traveljournal.domain.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
